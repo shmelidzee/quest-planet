5e6f2f8d8cfbb44df516de0e49cccbb77283e132
Paul <cr7mks1@mail.ru>
2022-12-13 09:57:55 +0300

    add info, refactoring code

---
+++ src/main/java/servlets/LossServlet.java
@@ -0,0 +1,16 @@
+package servlets;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "LossServlet", value = "/loss")
+public class LossServlet extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+    }
+}
